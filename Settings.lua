50fe249dd8d63c4f24313b542b5deeb8cc0ae5b5
RipeRubies <36025566+RipeRubies@users.noreply.github.com>
2018-02-24 18:40:45 -0500

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,1 +1,16 @@
-
+Settings = setmetatable({
+		getDefault = function(classname)
+			return classname and getmetatable(Pineapple.Settings).Default[classname] or getmetatable(Pineapple.Settings).Default
+		end;
+		setDefault = function(classname,properties)
+			Pineapple.Settings.getDefault()[classname] = properties
+		end;
+		
+		new = function(obj,name,defaultvalue)
+			
+		},{
+		Default = {};
+		Settings = {};
+	}
+)
+  

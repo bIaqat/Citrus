948805babd01f7caad065f496604805b132fc893
RipeRubies <36025566+RipeRubies@users.noreply.github.com>
2018-02-27 23:32:29 -0500

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -3,11 +3,28 @@ Settings = setmetatable({
 			return classname and getmetatable(Pineapple.Settings).Default[classname] or getmetatable(Pineapple.Settings).Default
 		end;
 		setDefault = function(classname,properties)
-			Pineapple.Settings.getDefault()[classname] = properties
+			getmetatable(Pineapple.Settings).Default[classname] = properties;
 		end;
-		
-		new = function(obj,name,defaultvalue)
-			
+		newList = function(name)
+			getmetatable(Pineapple.Settings).Settings[name] = {};
+		end;
+		getList = function(name)
+			return getmetatable(Pineapple.Settings).Settings[name] or {}
+		end;
+		new = function(list,name,object,index,defaultval)
+			local list = Pineapple.Settings.getList(list)
+			list[name] = {[object] = index, Value = defaultval or object[index], Default = defaultval;
+				Set = function(self,newval)
+					self.Value = newval
+					local i,v = next(self)
+					i[v] = newval
+				end;
+				toDefault = function(self)
+					self:Set(self.Default)
+				end;
+			}
+			return list
+		end;
 		},{
 		Default = {};
 		Settings = {};

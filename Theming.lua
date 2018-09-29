26f85766a6b4357dd2f6045859ef990fcc2ac9d2
Karmal <roug2497@gmail.com>
2018-09-29 11:41:11 -0400

    Theming | SyncFunctions
    Changed algorithm of theming to A. delete old objects if set to other themes and B. set the new value from theme.SyncFunction so the sync function can be customized instead of a[Property] = b

--- Theming.lua
+++ Theming.lua
@@ -1,12 +1,16 @@
 Theming = setmetatable({
 	Themes = {}
 	},{
+	Objects = {};
 	__index = function(self,index)
 		local gelf,ret = getmetatable(self)
 		gelf.__index = {}
 		for i,v in next, {
 			new = function(name,...)
 				local theme = {
+					SyncFunction = function(self, obj, prop, val)
+						obj[prop] = val
+					end;
 					AutoSync = true;
 					Name = name;
 					Values = setmetatable({...},{
@@ -23,14 +27,42 @@ Theming = setmetatable({
 						end;
 					});
 					Objects = {};
-					Set = function(me,...)
-						self.setTheme(me,...)
+					Set = function(theme,...)
+						local args = {...}
+						if #args == 2 and type(args[2]) == 'number' then
+							theme.Values[args[2]] = args[1]
+						else
+							theme.Values = setmetatable({...},getmetatable(theme.Values))
+						end
+						local run = theme.AutoSync and theme:Sync()
 					end;
-					Insert = function(me,...)
-						self.insertObject(me,...)
+					Insert = function(theme,obj,prop,ind)
+						local objs = getmetatable(self)
+						if objs[obj] then
+							self.Themes[objs[obj]].Objects[obj][prop] = nil
+						end
+						local value = theme.Values(ind,type(obj[prop]))
+						if not theme.Objects[obj] then
+							theme.Objects[obj] = {}
+						end
+						theme.Objects[obj][prop] = ind or 1
+						obj[prop] = theme.AutoSync and value or obj[prop]
+						objs[obj] = name;
 					end;
-					Sync = function(me,...)
-						self.sync(me,...)
+					Sync = function(name,lerp,tim,...)
+						for obj,v in next, name.Objects do
+							for prop,ind in next, v do
+								local value = name.Values(ind,type(obj[prop]))
+								if not lerp then
+									name:SyncFunction(obj, prop, value)
+								else
+									game:GetService('TweenService'):Create(obj,TweenInfo.new(tim or 1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out,...),{[prop] = value}):Play()
+								end
+							end
+						end						
+					end;
+					setSync = function(me, func)
+						me.SyncFunction = func
 					end;
 				}
 				self.Themes[name] = theme
@@ -42,42 +74,26 @@ Theming = setmetatable({
 			end;
 			setTheme = function(name,...)
 				local theme = type(name) == 'table' and name or self.getTheme(name)
-				local args = {...}
-				if #args == 2 and type(args[2]) == 'number' then
-					theme.Values[args[2]] = args[1]
-				else
-					theme.Values = setmetatable({...},getmetatable(theme.Values))
-				end
-				local run = theme.AutoSync and theme:Sync()
+				theme:Set(...);
 			end;
-			insertObject = function(name,obj,prop,ind)
+			insertObject = function(name,...)
 				local theme = type(name) == 'table' and name or self.getTheme(name)
-				local value = theme.Values(ind,type(obj[prop]))
-				if not theme.Objects[obj] then
-					theme.Objects[obj] = {}
-				end
-				theme.Objects[obj][prop] = ind or 1
-				obj[prop] = theme.AutoSync and value or obj[prop]
+				theme:Insert(...)
 			end;
-			sync = function(name,lerp,tim,...)
+			sync = function(name,...)
 				if not name then
 					for i,v in next, self.Themes do
 						self.sync(v)
 					end
 				else
 					name = type(name) == 'table' and name or self.getTheme(name)
-					for obj,v in next, name.Objects do
-						for prop,ind in next, v do
-							local value = name.Values(ind,type(obj[prop]))
-							if not lerp then
-								obj[prop] = value
-							else
-								game:GetService('TweenService'):Create(obj,TweenInfo.new(tim or 1,Enum.EasingStyle.Quad,Enum.EasingDirection.Out,...),{[prop] = value}):Play()
-							end
-						end
-					end
+					name:Sync(...)
 				end
-			end			
+			end;
+			setSyncFunction = function(theme, func)
+				theme = type(theme) == 'table' and theme or self.getTheme(theme)
+				theme:setSync(func)
+			end;
 		} do
 			gelf.__index[i] = v
 			if i == index then ret = v end
